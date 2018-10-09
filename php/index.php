e78ea586a0ddbe06b6faec6a8c5516c6678e6b68
Henrique Ritter <36273680+henriqueritter@users.noreply.github.com>
2018-10-09 14:28:25 -0300

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Página Inicial - Monitor de Hosts</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	</head>
+	<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+		<table id="Table_01" width="1360" height="683" border="0" cellpadding="0" cellspacing="0">
+			<tr>
+				<td>
+					<a href="cadastrar.php" 
+						onmouseover="window.status='Cadastros';  return true;"
+						onmouseout="window.status='';  return true;">
+						<img src="images/CADASTRAR.gif" width="376" height="88" border="0" alt="">
+					</a>
+				</td>
+				<td>
+					<a href="monitor.php">
+						<img src="images/MONITOR.gif" width="399" height="88" border="0" alt="Monitor de Hosts">
+					</a>
+				</td>
+				<td rowspan="2">
+					<img src="images/index.php_04.gif" width="585" height="682" alt="">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<img src="images/index.php_05.gif" width="775" height="594" alt="">
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
